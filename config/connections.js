5f2b1062fd926876e73f095e92db04f7e2b3a9be
Michael Wolf <Michael Wolf>
2017-04-28 22:11:41 -0400

    prepared heroku deploy jawsdb

--- config/connections.js
+++ config/connections.js
@@ -1,26 +1,31 @@
 /* Connection to the database and export and used by the O.R.M. */
-var mysql = require('mysql');
+var mysql = require('mysql'),
+    connection,
+    source = {
+        localhost: {
+            port: 3306,
+            host: 'localhost',
+            user: 'root', // Your MySql username
+            password: 'root', // Your MySql password
+            database: 'burgers_db' // Run schema and seeds in burger/db directory
+        },
+        jawsDB: {
+            port: 3306,
+            host: 'otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            user: 'zc4fhf9asxxwdtbt',
+            password: 'ane9ogwbjgcsviwn',
+            database: 'burgers_db'
+        }
+    };
 
-var source = {
-
-    localhost: {
-        port: 3306,
-        host: 'localhost',
-        user: 'root', // Your MySql username
-        password: 'root', // Your MySql password
-        database: 'burgers_db' // Run schema and seeds in burger/db directory
-    },
-    jawsDB: {
-        port: 3306,
-        host: 'otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'zc4fhf9asxxwdtbt',
-        password: 'ane9ogwbjgcsviwn',
-        database: 'burgers_db'
-    }
+// connect to localhost if jawsdb doesn't exist
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(source.jawsDB);
+} else {
+    connection = mysql.createConnection(source.localhost);
 }
 
-var connection = mysql.createConnection(source.jawsDB);
-
+// Connect!!
 connection.connect(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
