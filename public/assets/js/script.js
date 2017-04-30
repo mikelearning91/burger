bc89fe64a2faad2871a5156cf3feb9a5191d1d84
Michael Wolf <Michael Wolf>
2017-04-30 00:28:15 -0400

    css, delete buttons modified, spacing of containers modified

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,17 @@
 var isFirst = true,
     num = 1,
-    burgerItem = ".burger-item";
+    burgerItem = ".burger-item",
+    burgerDevouredItem = ".burger-devoured-item";
 
+// Counter for burgers to be devoured
 $(burgerItem).each(function() {
+    var list = $(this);
+    list.attr("start", num);
+    num = num + list.find("span").length;
+});
+
+// Counter for devoured burgers
+$(burgerDevouredItem).each(function() {
     var list = $(this);
     list.attr("start", num);
     num = num + list.find("span").length;
